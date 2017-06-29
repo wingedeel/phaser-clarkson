47708a045d71995ed56604b53d8054b3d53c873f
Anne Kotecha <anne.kotecha@gmail.com>
2017-06-29 17:09:40 +0100

    Added score to Game Over screen.

--- games/hungryDragon/js/StateOver.js
+++ games/hungryDragon/js/StateOver.js
@@ -8,6 +8,17 @@ var StateOver={
     
     create:function()
     {
+        this.top=0;
+        this.bottom = game.height-120;
+
+        // IPAD FIX 
+        if (screen.height > 764) {
+            // ipad height is 755
+            // centers y pos. Will look like a wide screen film
+            this.background.y = game.world.centerY-this.background.height/2;
+            this.top = this.background.y;
+            this.bottom = this.background.y+360;
+        }
         this.btnPlayAgain = game.add.button(
        		game.world.centerX,
        		game.world.centerY+100,
@@ -26,6 +37,17 @@ var StateOver={
         this.dragon.animations.add('fly', [0, 1, 2, 3], 12, true);
         this.dragon.animations.play('fly');
         this.dragon.scale.x =-1;
+
+         // SCORE TEXT
+       this.scoreText = game.add.text(game.world.centerX, this.top+60, score);
+       this.scoreText.fill = "#000000";
+       this.scoreText.fontSize =  64;
+       this.scoreText.anchor.set(0.5, 0.5);
+
+       this.scoreLabel = game.add.text(game.world.centerX, this.top+20, "Score");
+       this.scoreLabel.fill = "#000000";
+       this.scoreLabel.fontSize =  32;
+       this.scoreLabel.anchor.set(0.5, 0.5);
 
         // GAME BACKGROUND
         game.stage.backgroundColor="#26C9FF";
